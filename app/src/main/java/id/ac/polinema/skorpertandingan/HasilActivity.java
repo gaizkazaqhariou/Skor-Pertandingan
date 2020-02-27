6f0a499ebe8bffc681d6443787f91a9868f4b37b
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-27 23:28:49 +0700

    First Commit. Making 3 activity.

---
+++ app/src/main/java/id/ac/polinema/skorpertandingan/HasilActivity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.skorpertandingan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HasilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+    }
+}
