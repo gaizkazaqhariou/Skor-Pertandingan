998cc2283ea8500028ef2dddfd0e65261f68bbd1
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-28 01:14:00 +0700

    Second commit. make view linked each other

--- app/src/main/java/id/ac/polinema/skorpertandingan/MainActivity.java
+++ app/src/main/java/id/ac/polinema/skorpertandingan/MainActivity.java
@@ -1,14 +1,28 @@
 package id.ac.polinema.skorpertandingan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mNo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btN = findViewById(R.id.btNext);
+        btN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScoreMain.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/id/ac/polinema/skorpertandingan/ScoreMain.java
+++ app/src/main/java/id/ac/polinema/skorpertandingan/ScoreMain.java
@@ -1,14 +1,70 @@
 package id.ac.polinema.skorpertandingan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ScoreMain extends AppCompatActivity {
+
+//    private int mNo;
+//    private Fragment fg = new Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_main);
+
+        BlankFragment frag = new BlankFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, frag, "Test Fragment");
+        transaction.commitNow();
+
+        Button btG = findViewById(R.id.btToHasil);
+        btG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScoreMain.this, HasilActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+//    private void changePage(int mNo) {
+//        //changeNumber(mNo);
+//        changeFragment(mNo);
+//    }
+//
+//    private void initSelector() {
+//        findViewById(R.id.addAway).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (mNo>1){
+//                    changePage(--mNo);
+//                }
+//            }
+//        });
+//        findViewById(R.id.addHome).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (mNo<2){
+//                    changePage(++mNo);
+//                }
+//            }
+//        });
+//    }
+
+//    private void changeFragment(int mNo) {
+//        FragmentManager fm = getSupportFragmentManager();
+//        Class<BlankFragment> fragment= null;
+//        switch (mNo)
+//        {
+//            case 1 : fm.beginTransaction().replace(R.id.container, BlankFragment).commitNow(); break;
+//        }
+//    }
 }
