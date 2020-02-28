d69901133acaa1fd725626835cf5c959eeb19802
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-28 11:03:09 +0700

    sampek ngirim intent ke scoremain

--- app/src/main/java/id/ac/polinema/skorpertandingan/MainActivity.java
+++ app/src/main/java/id/ac/polinema/skorpertandingan/MainActivity.java
@@ -1,28 +1,88 @@
 package id.ac.polinema.skorpertandingan;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int mNo;
-
+    static final int REQUEST_IMAGE_GET1 = 1;
+    static final int REQUEST_IMAGE_GET2 = 2;
+    ImageView ivFotoA;
+    ImageView ivFotoH;
+    Uri uriFoto1;
+    Uri uriFoto2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText teamHome = findViewById(R.id.etNamaHome);
+        final EditText teamAway = findViewById(R.id.etNamaAway);
+        ivFotoA = findViewById(R.id.ivInAway);
+        ivFotoH = findViewById(R.id.ivInHome);
+
+        ivFotoA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vA) {
+                pickPhotoA(vA);
+            }
+        });
+        ivFotoH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vH) {
+                pickPhotoH(vH);
+            }
+        });
 
         Button btN = findViewById(R.id.btNext);
         btN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ScoreMain.class);
+                intent.putExtra("timHome", teamHome.getText());
+                intent.putExtra("timAway", teamAway.getText());
+
+                ivFotoA.buildDrawingCache();
+                ivFotoH.buildDrawingCache();
+                Bitmap bitmap1 = ivFotoA.getDrawingCache();
+                Bitmap bitmap2 = ivFotoH.getDrawingCache();
+                intent.putExtra("BitmapAway", bitmap1);
+                intent.putExtra("BitmapHome", bitmap2);
+
                 startActivity(intent);
             }
         });
+    }
+
+    private void pickPhotoA(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GET1);
+    }
+
+    private void pickPhotoH(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GET2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_GET1) {
+            uriFoto1 = data.getData();
+            ivFotoA.setImageURI(uriFoto1);
+        }
+        if (requestCode == REQUEST_IMAGE_GET2) {
+            uriFoto2 = data.getData();
+            ivFotoH.setImageURI(uriFoto2);
+        }
     }
 }
--- app/src/main/java/id/ac/polinema/skorpertandingan/ScoreMain.java
+++ app/src/main/java/id/ac/polinema/skorpertandingan/ScoreMain.java
@@ -1,11 +1,14 @@
 package id.ac.polinema.skorpertandingan;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -19,11 +22,33 @@ public class ScoreMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_main);
 
-        BlankFragment frag = new BlankFragment();
+        final BlankFragment frag = new BlankFragment();
         FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, frag, "Test Fragment");
-        transaction.commitNow();
+        final FragmentTransaction transaction = manager.beginTransaction();
+
+        Bundle extras = getIntent().getExtras();
+//sadbhsagdashgsa
+        Bitmap bitmapH = extras.getParcelable("BitmapAway");
+        ImageView ivH = findViewById(R.id.ivInHome);
+        ivH.setImageBitmap(bitmapH);
+
+        if (savedInstanceState == null) {
+            transaction.add(R.id.container, frag, "Default");
+            transaction.commitNow();
+        }
+
+        findViewById(R.id.addHome).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vH) {
+                loadFragment(new addHome());
+            }
+        });
+        findViewById(R.id.addAway).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vA) {
+                loadFragment(new addAway());
+            }
+        });
 
         Button btG = findViewById(R.id.btToHasil);
         btG.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +58,13 @@ public class ScoreMain extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void loadFragment(Fragment fr) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fr);
+        fragmentTransaction.commit();
     }
 
 //    private void changePage(int mNo) {
